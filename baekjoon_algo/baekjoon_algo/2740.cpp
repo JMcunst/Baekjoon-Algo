7975b421d7c9315e348f0c3862ad3f9942ba99df
JMcunst <xnslqjtmghaz@naver.com>
2021-03-13 18:22:21 +0900

    baekjoon 2740, div&conq06, 2021_0127, 행렬 곱셈

---
+++ baekjoon_algo/baekjoon_algo/2740.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+int matrixA[MAX][MAX], matrixB[MAX][MAX];
+int result[MAX][MAX];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M, K;
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> matrixA[i][j];
+
+    cin >> M >> K;
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < K; j++)
+            cin >> matrixB[i][j];
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < K; j++)
+            for (int k = 0; k < M; k++)
+                result[i][j] += (matrixA[i][k] * matrixB[k][j]);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < K; j++)
+            cout << result[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+
+}
