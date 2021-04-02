c675873a702dfba0e555eee713c8a0d30a1d46ea
JMcunst <xnslqjtmghaz@naver.com>
2021-04-02 17:21:49 +0900

    baekjoon 1300, bisearch_06, 2021_0210,K번째 수

--- python/1300.py
+++ python/1300.py
@@ -6,8 +6,8 @@ var_k = int(stdin.readline())
 def count_value(mid, N):       #해당 mid값의 번째를 count하는 함수
     temp_count = 0
 
-    for x in range(1, N+1):                # key algorithm
-       temp_count += min(N, mid//x)
+    for x in range(1, N+1):                 # key algorithm
+       temp_count += min(N, mid//x)         # 4//1->3 , 4//2->2 , 4//3->1 , 4//4->1
     
     return temp_count
 
@@ -15,7 +15,7 @@ def binary_k(var_N, var_k):
     high, low = var_k, 1
 
     while low <= high:
-        mid = (high + low)//2
+        mid = (high + low)//2                # mid = 4
 
         count = count_value(mid, var_N)
 
