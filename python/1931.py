7b34da8dee3af477e25b2780ae00b076f38ce001
JMcunst <xnslqjtmghaz@naver.com>
2021-04-09 17:23:28 +0900

    baekjoon 1931, greedy_02, 210224, 회의실배정

--- python/1931.py
+++ python/1931.py
@@ -8,11 +8,13 @@ for _ in range(var_N):
     list_meeting.append([var_start,var_end])
 
 list_meeting = sorted(list_meeting, key = lambda x : (x[1], x[0]))
+# x[0] : 0 3 0 5 3 5 6  8  8  2  12
+# x[1] : 4 5 6 7 8 9 10 11 12 13 14
 
-rtn = end = 0
-for i,j in list_meeting:
-    if i >= end:
-        rtn += 1
-        end = j
+rtn = end = 0               # i  0 3 0 5 3 5 6  8  8  2  12
+for i,j in list_meeting:    # j  4 5 6 7 8 9 10 11 12 13 14
+    if i >= end:            #    1>=0 3>=4 0>=4 5>=4 6>=7 8>=7 8>=11 2>=11 12>=11   
+        rtn += 1            #rtn   1              2         3                4
+        end = j             #end   4              7         11               14
 
 print(rtn)
