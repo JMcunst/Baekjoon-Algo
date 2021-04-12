5162a3a576edb68af45e4856527bf92b8d8d08c4
JMcunst <xnslqjtmghaz@naver.com>
2021-04-12 15:46:00 +0900

    baekjoon 11399, greedy_03, 210225, ATM

--- python/11399.py
+++ python/11399.py
@@ -2,12 +2,13 @@ from sys import stdin
 
 var_N = int(stdin.readline())
 list_waiting_time = list(map(int, stdin.readline().split()))
-
+# [3, 1, 4, 3, 2]
 list_waiting_time.sort()
+# [1, 2, 3, 3, 4]
 
-rtn = var_wait = 0
-for x in list_waiting_time:
-    var_wait += x
-    rtn += var_wait
-
+rtn = var_wait = 0                    # 1
+for x in list_waiting_time:           # 1 2
+    var_wait += x                     # 1 2 3
+    rtn += var_wait                   # 1 2 3 3
+                                      # 1 2 3 3 4 
 print(rtn) 
