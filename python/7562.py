c78e82abea53f6e06cd85d29e37aed95de8c9cc9
JMcunst <xnslqjtmghaz@naver.com>
2021-04-27 17:03:51 +0900

    baekjoon 7562, dfs&bfs_09, 210314, 나이트의이동(BFS)

--- python/7562.py
+++ python/7562.py
@@ -1,8 +1,8 @@
 from sys import stdin
 from collections import deque
 
-dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-dy = [2, 1, -1, -2, -2, -1, 1, 2]
+dx = [1, 2, 2, 1, -1, -2, -2, -1]
+dy = [-2, -1, 1, 2, 2, 1, -1, -2]
 
 def bfs(var_x,var_y,var_tx,var_ty):
     queue = deque()
@@ -17,7 +17,7 @@ def bfs(var_x,var_y,var_tx,var_ty):
         for i in range(8):
             nx = var_nx + dx[i]
             ny = var_ny + dy[i]
-            if 0<=nx<var_N and 0<=ny<var_N and mat_field[nx][ny] == 0:
+            if 0 <= nx < var_N and 0 <= ny < var_N and mat_field[nx][ny] == 0:
                 queue.append([nx,ny])
                 mat_field[nx][ny] = mat_field[var_nx][var_ny] + 1
 
@@ -29,4 +29,3 @@ for _ in range(var_T):
     var_tx,var_ty = map(int, stdin.readline().split())
     mat_field = [[0] * var_N for i in range(var_N)]
     bfs(var_x,var_y,var_tx,var_ty)
-    
