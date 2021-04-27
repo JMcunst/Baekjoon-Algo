c40fb7113c7c59c29cf52a6de3c7eb653b8b3894
JMcunst <xnslqjtmghaz@naver.com>
2021-04-27 15:51:18 +0900

    baekjoon 2206, dfs&bfs_08, 210313, 벽부수고이동하기(BFS)

--- python/2206.py
+++ python/2206.py
@@ -1,32 +1,37 @@
 from sys import stdin
 from collections import deque
+import pprint
 
-dx=[-1,0,1,0] # 좌 우 위 아래
-dy=[0,1,0,-1] # 좌 우 위 아래
+dx=[-1,0,1,0] # 좌 우 
+dy=[0,1,0,-1] # 위 아래
 
 def bfs():
-    q.append([0, 0, 0])
+    queue.append([0, 0, 0])
     mat_visit[0][0][0] = 1
-    while q:
-        x, y, z = q.popleft()
+    while queue:
+        x, y, z = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < var_N and 0 <= ny < var_M:
-                if mat_before[nx][ny] == 0 and mat_visit[nx][ny][z] == -1:
+                if mat_before[nx][ny] == 0 and mat_visit[nx][ny][z] == -1:  # 좌우위아래 길이고, 방문한적이 없다면,
                     mat_visit[nx][ny][z] = mat_visit[x][y][z] + 1
-                    q.append([nx, ny, z])
-                elif z == 0 and mat_before[nx][ny] == 1 and mat_visit[nx][ny][z+1] == -1:
+                    queue.append([nx, ny, z])
+                    #print(queue)
+                elif z == 0 and mat_before[nx][ny] == 1 and mat_visit[nx][ny][z+1] == -1: # z==0:뚫은적이 없고, 좌우위아래벽이고, 방문한적이 없다면,
                     mat_visit[nx][ny][z+1] = mat_visit[x][y][z] + 1
-                    q.append([nx, ny, z+1])
+                    queue.append([nx, ny, z+1])
+                    #print(queue)
 
 var_N,var_M = map(int, stdin.readline().split())
 mat_before = [list(map(int, input())) for _ in range(var_N)]
+#mat_before = [list(map(int, stdin.readline().split())) for _ in range(var_N)]   -> why?
 mat_visit = [[[-1]*2 for _ in range(var_M)] for _ in range(var_N)]
-q = deque()
-
+queue = deque()
+#pprint.pprint(mat_visit)
 bfs()
-
+print(mat_visit[0][2][0], mat_visit[0][2][1])
+#pprint.pprint(mat_visit)
 var_rtn_1, var_rtn_2 = mat_visit[var_N-1][var_M-1][0], mat_visit[var_N-1][var_M-1][1]
 if var_rtn_1 == -1 and var_rtn_2 != -1:
     print(var_rtn_2)
@@ -34,4 +39,3 @@ elif var_rtn_1 != -1 and var_rtn_2 == -1:
     print(var_rtn_1)
 else:
     print(min(var_rtn_1, var_rtn_2))
-
