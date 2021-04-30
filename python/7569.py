e8e61d3bf9cd5751a573ede91cda4268ba581c56
JMcunst <xnslqjtmghaz@naver.com>
2021-04-30 22:50:07 +0900

    baekjoon 7569, dfs&bfs_11, 210426, 토마토(BFS)

--- python/7569.py
+++ python/7569.py
@@ -5,7 +5,6 @@ dx = [1, -1, 0, 0, 0, 0]
 dy = [0, 0, -1, 1, 0, 0]
 dz = [0, 0, 0, 0, -1, 1]
 
-matrix = []
 queue = deque()
 
 
@@ -13,7 +12,14 @@ queue = deque()
 
 
 var_M, var_N, var_H = map(int, stdin.readline().split())
+matrix = [[] for i in range(var_H)]
+isTrue = False
+st = False
 
 for i in range(var_H):
     for j in range(var_N):
-        matrix.append(list(map(int, stdin.readline().split())))
+        matrix[i].append(list(map(int, stdin.readline().split())))
+
+
+visit = [[[0 for i in range(var_M)] for i in range(var_N)] for i in range(var_H)]
+
