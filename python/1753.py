6cfa02c03db61f4707f72e4f1ff9dcc5ecbe256e
JMcunst <xnslqjtmghaz@naver.com>
2021-05-03 17:54:02 +0900

    baekjoon 1753, 최단경로_01, 210317, 최단경로(다익스트라)

--- python/1753.py
+++ python/1753.py
@@ -3,20 +3,20 @@ from heapq import heappush, heappop
 INF = 1e9
 
 def dijkstra(var_start):
-    list_shortest_distance[var_start] = 0
+    list_shortest_dist[var_start] = 0
     heappush(heap, [0, var_start])
-    while heap:                                     # [[0, 1]]
-        vtx_w, vtx_v = heappop(heap)                # [[2, 2], [3, 3]]
-        for now_v, wei in mat_table[vtx_v]:         # [[3, 3], [7, 4]]
-            now_w = wei + vtx_w                     # [[7, 4]]
-            if now_w < list_shortest_distance[now_v]:
-                list_shortest_distance[now_v] = now_w
+    while heap:                                                                                 # [[0, 1]]
+        pre_w, vtx_v = heappop(heap)                                                            # [[2, 2], [3, 3]]
+        for now_v, vtx_w in mat_table[vtx_v]:  #해당 vtx_v이 갈 수 있는 vertex들의 정보 꺼내옴      # [[3, 3], [7, 4]]
+            now_w = vtx_w + pre_w                                                                 # [[7, 4]]
+            if now_w < list_shortest_dist[now_v]:
+                list_shortest_dist[now_v] = now_w
                 heappush(heap, [now_w, now_v])
 
 var_V,var_E = map(int, stdin.readline().split())
 var_start = int(stdin.readline())
 mat_table = [[] for i in range(var_V+1)]     # [[], [], [], []] => [[], [[2, 2], [3, 3]], [[3, 4], [4, 5]], [[4, 6]], [], [[1, 1]]]
-list_shortest_distance = [INF] * (var_V + 1) # [1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0]
+list_shortest_dist = [INF] * (var_V + 1) # [1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0, 1000000000.0]
 heap = []
 
 for _ in range(var_E):
@@ -25,5 +25,5 @@ for _ in range(var_E):
 
 dijkstra(var_start)
 
-for i in list_shortest_distance[1:]:
+for i in list_shortest_dist[1:]:
     print(i if i != INF else "INF")
