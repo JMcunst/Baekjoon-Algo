48984be724c34dc660b84120d76f69afc0762e38
JMcunst <xnslqjtmghaz@naver.com>
2021-05-06 00:27:09 +0900

    baekjoon 9370, 최단경로_03, 210322, 미확인도착지

--- python/9370.py
+++ python/9370.py
@@ -6,6 +6,6 @@ for _ in range(var_T):
     var_n, var_m, var_t = map(int, stdin.readline().split()) # n:교차로, m:도로, t:목적지후보
     var_s, var_g, var_h = map(int, stdin.readline().split()) # s:예술가출발, g:지점1 h:지점2
     for _ in range(var_m):
-        var_a, var_b, var_d = map(int, stdin.readline().split())
+        var_a, var_b, var_d = map(int, stdin.readline().split()) # a와 b사이에 길이 d의 양방향 도로
     for _ in range(var_t):
-        var_x = int(stdin.readline())
+        var_x = int(stdin.readline()) # t개의 목적지 후보들, 서로 다른 위치며, 모두 s가 아니다.
