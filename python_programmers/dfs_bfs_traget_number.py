fc203d885c978a09f9565b27b4178a80b8cf37ee
Jmcunst <xnslqjtmghaf@gmail.com>
2022-02-07 20:56:47 +0900

    programmers DFSBFS, 20220207, 타겟넘버

--- python_programmers/dfs_bfs_traget_number.py
+++ python_programmers/dfs_bfs_traget_number.py
@@ -1,11 +1,18 @@
 from collections import deque
+
+numbers = [1,1,1,1,1]
+target = 3
+
 def solution(numbers, target):
     answer = 0
     queue = deque()
     queue.append([numbers[0],0])
     queue.append([-1*numbers[0],0])
+    print('orgin queue::',queue)
     while queue:
+        print('next queue::',queue)
         temp, idx = queue.popleft()
+        print('temp::',temp,'idx::', idx)
         idx += 1
         if idx < len(numbers):
             queue.append([temp+numbers[idx], idx])
@@ -14,3 +21,5 @@ def solution(numbers, target):
             if temp == target:
                 answer += 1
     return answer
+
+solution(numbers, target)
