6a4650a054037c659f3f062821d4de45af51d044
Jmcunst <xnslqjtmghaf@gmail.com>
2022-02-08 15:51:07 +0900

    programmers 정렬, 20220208, 가장 큰 수

---
+++ python_programmers/sort_big_number.py
@@ -0,0 +1,28 @@
+numbers = [6, 10, 2]
+
+def solution(numbers):
+    answer = ''
+    num = list(map(str, numbers)) 
+    print(num)
+    num.sort(key = lambda x : x*3, reverse = True) 
+    print(num)
+    
+    answer = str(int(''.join(num)))
+    print(answer)
+    
+    return answer
+
+solution(numbers)
+
+import functools
+
+# def comparator(a,b):
+#     t1 = a+b
+#     t2 = b+a
+#     return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
+# def solution(numbers):
+#     n = [str(x) for x in numbers]
+#     n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+#     answer = str(int(''.join(n)))
+#     return answer
