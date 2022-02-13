6eb9798a0daa46332700e196187729d904a9b4f7
Jmcunst <xnslqjtmghaf@gmail.com>
2022-02-13 17:07:04 +0900

    programmers 해쉬, 20220213, 완주하지 못한 선수

---
+++ python_programmers/hash_not_finish_athlete.py
@@ -0,0 +1,33 @@
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+def solution(participant, completion):
+    hashDict = {}
+    sumHash = 0 
+    
+    # 1. Hash : Participant의 dictionary 만들기 
+    # # 2. Participant의 sum(hash) 구하기 
+    for part in participant:
+        print('part::', part)
+        hashDict[hash(part)] = part
+        print('hashDict::', hashDict)
+        sumHash += hash(part) 
+        print('sumHash::', sumHash)
+
+    print('total hashDict::', hashDict)
+        
+    # 3. completion의 sum(hash) 빼기 
+    for comp in completion: sumHash -= hash(comp)
+    
+    # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다
+    return hashDict[sumHash]
+    
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"] , ["josipa", "filipa", "marina", "nikola"]))
+
+
+# import collections
+
+
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
