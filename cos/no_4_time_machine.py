83a5accf4085853f4b2b6c3a18596ea2dee08d04
Jmcunst <xnslqjtmghaf@gmail.com>
2022-02-22 12:40:58 +0900

    COS_1차 타임머신, 20220222, digit logic

---
+++ cos/no_4_time_machine.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def solution(num):
+	answer = num + 1
+	digit = 1
+	while answer // digit % 10 == 0:
+		answer += digit
+		digit *= 10
+	return answer
+
+num = 9949999;
+ret = solution(num)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
