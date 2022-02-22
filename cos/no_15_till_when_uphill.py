a79f261673f7c5a8be8ebc34610d5c5f654f933f
Jmcunst <xnslqjtmghaf@gmail.com>
2022-02-22 17:01:54 +0900

    COS_1차 언제까지오르막길, 20220222, logic(idx배열)

---
+++ cos/no_15_till_when_uphill.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def solution(arr):
+    answer = 0
+    idx = 0
+
+    tmp = [0 for i in range(len(arr)-1)]
+
+    for i in range(len(arr)-1):
+        if arr[i] < arr[i+1]:
+            tmp[idx] += 1
+        else:
+            idx += 1
+
+    answer = max(tmp)+1
+
+    if answer < 2:
+        return 1
+        
+    return answer
+
+
+arr = [3, 1, 2, 4, 5, 1, 2, 2, 3, 4]
+ret = solution(arr)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
