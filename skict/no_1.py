efbe6edc1b82c234e06321342f4fe36de689972f
Jmcunst <xnslqjtmghaf@gmail.com>
2022-03-12 18:05:20 +0900

    SK ICT, 1번 문제, 원가 대비 가격

---
+++ skict/no_1.py
@@ -0,0 +1,26 @@
+# 1번 문제
+# 원가 대비 가격에 대한 그리디
+
+from collections import Counter
+
+
+def solution(money, costs):
+    answer = 0
+    counter = {}
+    
+    pr_list = [(1, costs[0]), (5,costs[1]), (10,costs[2]), (50,costs[3]),(100,costs[4]),(500,costs[5])]
+    pr_list.sort(key=lambda x : x[0]/x[1], reverse=True)
+
+
+    for value, cost in pr_list:
+        share = money // value
+        counter[cost] = share
+        money %= value
+        print(share, money)   
+
+
+    for value, cnt in counter.items():
+        cost = value * cnt
+        answer += cost
+    
+    return answer
