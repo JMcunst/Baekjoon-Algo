23353800128fbf3d38045240eb0ea826a7dadf9b
JMcunst <xnslqjtmghaf@gmail.com>
2023-04-06 20:56:00 +0900

    swea : 점심식사시간 (Brute Force, 완전탐색)

---
+++ swea/05_2383_lunch_time.py
@@ -0,0 +1,117 @@
+#------------answer1-------------#
+T = int(input())+1
+for tc in range(1,T):
+    N = int(input())
+    arr = [list(map(int, input().split())) for i in range(N)]
+
+    queue = []
+    e = []
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                queue.append([i, j])
+            if arr[i][j] > 1:
+                e.append([i, j, arr[i][j]])
+    D = []
+
+    for j in queue:
+        a = []
+        for i in e:
+            dis = abs(i[0] - j[0]) + abs(i[1] - j[1]) + 1
+            a.append(dis + i[2])
+        print('AA::',a)
+        D.append(a)
+    print('DDD::',D)
+    res = []
+    r1 = 0
+    r2 = 0
+    m_i = 0
+    for i in D:
+        if i[0] < i[1]:
+            r1 += 1
+            res.append([i[0], 1, m_i])
+        else:
+            r2 += 1
+            res.append([i[1], 2, m_i])
+        m_i += 1
+    res.sort()
+    print('RES::',res)
+    for i in res:
+        if r1 > 3:
+            i[0] = min(i[0] + e[0][2], D[i[2]][1])
+            r1 -= 1
+        if r2 > 3:
+            i[0] = min(i[0] + e[1][2], D[i[2]][0])
+            r2 -= 1
+    r = 0
+    for i in res:
+        r = max(i[0], r)
+    print("#{} {}".format(tc,r))
+#------------------answer2---------------------#
+from itertools import combinations
+from collections import deque
+
+# 두 그룹으로 나누어 내려가는 시간 계산
+def go_for_lunch(people, stairs):
+    # 계단까지 도착하는 시간
+    s = []
+
+    for person in people:
+        s.append(abs(person[0]-stairs[0]) + abs(person[1]-stairs[1]))
+    
+    s = deque(sorted(s))
+
+    # 계단을 내려가서 도착할 시간
+    e = deque()
+
+    time = 0
+    curr = 0
+
+    while s:
+        time += 1
+
+        # 이동을 완료했을 경우
+        while e and e[0] == time:
+            e.popleft()
+            curr -= 1
+
+        while s[0] < time:
+            # 계단 이동
+            if curr < 3:
+                s.popleft()
+                if not s:
+                    time += grid[stairs[0]][stairs[1]]
+                    break
+
+                e.append(time+grid[stairs[0]][stairs[1]])
+                curr += 1
+            else:
+                break
+        
+    return time
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N = int(input())
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
+    people = []
+    stairs = []
+
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] == 1:
+                people.append((i, j))
+            elif grid[i][j] >= 2:
+                stairs.append((i, j))
+    
+    total = float('inf')
+    for n in range(N):
+        for people1 in combinations(people, n):
+            people2 = list(set(people) - set(people1))
+            time = max(go_for_lunch(people1, stairs[0]), go_for_lunch(people2, stairs[1]))
+            total = min(total, time)
+    
+    print('#{} {}'.format(t, total))
