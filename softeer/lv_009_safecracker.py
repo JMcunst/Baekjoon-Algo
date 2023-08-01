53e2a8cfa9a18a88b68580c84ef5d7221a0cea71
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-01 16:32:49 +0900

    lv2 : greedy,sort

---
+++ softeer/lv_009_safecracker.py
@@ -0,0 +1,18 @@
+import sys
+
+W, N = list(map(int, input().split()))
+
+metals = [list(map(int, input().split())) for _ in range(N)]
+
+metals.sort(key=lambda x : x[1], reverse=True)
+
+result = 0 
+
+for mw, mp in metals:
+    if W > mw:
+        result += mw * mp
+        W -= mw
+    else:
+        result += W * mp
+
+print(result)
